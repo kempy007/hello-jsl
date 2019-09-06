7fa181519468137c841e829850f3ecbf48e40685
Martyn Kemp <martyn@datasec1.co.uk>
2019-09-06 22:20:10 +0100

    Update getRandom.groovy

--- vars/getRandom.groovy
+++ vars/getRandom.groovy
@@ -3,6 +3,6 @@ import org.kempy.randomName
 
 def call(String name = 'world') {
     def rndName = new randomName()
-    def name = rndName.who()
+    name = rndName.who()
     echo "Hello there, ${name}!"
 }
