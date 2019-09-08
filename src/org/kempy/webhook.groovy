a34440b998aeb9e0d71f00e5563e9db2be815ac2
Martyn Kemp <martyn@datasec1.co.uk>
2019-09-08 12:09:53 +0100

    Update webhook.groovy

--- src/org/kempy/webhook.groovy
+++ src/org/kempy/webhook.groovy
@@ -1,13 +1,15 @@
 package org.kempy
 
-class webhook {
+class webhook implements Serializable {
+  def steps
+  webhook(steps) {this.steps = steps}
   def msg()
   {
     def url = "https://outlook.office.com/webhook/5de55542-4e21-4d48-9ea6-79f2c1fd3ef4@b945c813-dce6-41f8-8457-5a12c2fe15bf/IncomingWebhook/c545cc08906c4641ab6d879617cc5e1d/e151c1d1-ff94-48ce-96f1-9af1c39b9f45"
     def contentType = "\"Content-Type: application/json\""
     def message = "Hello World"
     def jsonWrap = "\"{\"text\": \"${message}\"}\""
-    sh "curl -H ${contentType} -d ${jsonWrap} ${url}"
+    steps.sh "curl -H ${contentType} -d ${jsonWrap} ${url}"
      return "Webhook sent"
   }
 
